2edebc435ff65ba1898ca6d44b8f8cdf2f1eb4a1
Ashley <yoojung131@gmail.com>
2026-04-29 18:23:12 +0900

    MacAddress 점검 코드 수정

--- PowerOn_test/macAddress/macAddress.ino
+++ PowerOn_test/macAddress/macAddress.ino
@@ -2,7 +2,14 @@
 
 void setup() {
     Serial.begin(115200);
+    
+    // 네이티브 USB 시리얼을 사용하는 보드에서 연결을 기다립니다.
+    while (!Serial) {
+        ; // 연결될 때까지 대기
+    }
+
     WiFi.mode(WIFI_STA);
+    Serial.println("\n--- WiFi MAC Address ---");
     Serial.print("MAC Address: ");
     Serial.println(WiFi.macAddress());
 }
