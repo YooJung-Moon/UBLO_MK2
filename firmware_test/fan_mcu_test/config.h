84ce82c1ff006152f702f36da334e8fa6d020f1b
Ashley <yoojung131@gmail.com>
2026-05-11 11:59:51 +0900

    limit switch 위치 수정

--- firmware_test/fan_mcu_test/config.h
+++ firmware_test/fan_mcu_test/config.h
@@ -5,10 +5,10 @@
 const uint8_t SENSOR_MCU_MAC[] = {0x20, 0x6E, 0xF1, 0x31, 0x25, 0x44};
 
 // 핀 정의
-#define MOTOR_IN1   D2
-#define MOTOR_IN2   D3
-#define LIM_OPEN    D5
-#define LIM_CLOSE   D6
+#define MOTOR_IN1   D3
+#define MOTOR_IN2   D2
+#define LIM_OPEN    D6
+#define LIM_CLOSE   D5
 #define FAN_PWM     D9
 
 // 타임아웃 (ms)
