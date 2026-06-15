38f6a887a296614b11556ce22fa466b1dad13aee
Ashley <yoojung131@gmail.com>
2026-06-15 10:55:53 +0900

    두 번째 mac id

--- firmware/fan_mcu/encoder.h
+++ firmware/fan_mcu/encoder.h
@@ -2,7 +2,9 @@
 #include <Arduino.h>
 
 void encoder_init();
-void encoder_update();   // loop()에서 항상 호출
+void encoder_update();
 bool encoder_changed();
 uint8_t encoder_get_mode();
 void encoder_set_mode(uint8_t mode);
+void encoder_error_blink();   // 커버 타임아웃 에러 시 호출
+bool encoder_is_error();      // 현재 에러 상태 확인
