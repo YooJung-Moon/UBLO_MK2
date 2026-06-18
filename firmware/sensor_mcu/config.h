20ee3d78fc3b31210e439af3f949f7f344f515b7
Ashley <yoojung131@gmail.com>
2026-06-18 15:08:17 +0900

    센서 2이상 페어링

--- firmware/sensor_mcu/config.h
+++ firmware/sensor_mcu/config.h
@@ -11,11 +11,11 @@ const uint8_t FAN_MCU_MAC[] = {0x20, 0x6E, 0xF1, 0x31, 0x2D, 0x34}; // pair 3
 #define CO2_DANGER      2000
 
 // 버퍼
-#define BUFFER_SIZE     3   // 테스트용: 3개 (10초 x 3 = 30초)
-// #define BUFFER_SIZE     30   // 최근 5분치 데이터 저장 (10초 × 30 = 300초 = 5분)
+// #define BUFFER_SIZE     3   // 테스트용: 3개 (10초 x 3 = 30초)
+#define BUFFER_SIZE     30   // 최근 5분치 데이터 저장 (10초 × 30 = 300초 = 5분)
 
-#define DECISION_COUNT  3    // 테스트용: 3개 (30초)
-// #define DECISION_COUNT  60   // 판단 주기: 10분 (10초 × 60 = 10분)
+// #define DECISION_COUNT  3    // 테스트용: 3개 (30초)
+#define DECISION_COUNT  30   // 판단 주기: 5분 (10초 × 30 = 5분)
 
 // 센서 수집 주기 (ms)
 #define SENSOR_INTERVAL 10000  // 10초
